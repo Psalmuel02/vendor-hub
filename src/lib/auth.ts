c8aca005ff15f1ef52964c06f9296839b0e773f1
Psalmuel02 <oladimejisamuel65@gmail.com>
2026-07-08 02:25:11 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,6 +13,7 @@ const credentialsSchema = z.object({
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: "jwt" },
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   pages: {
     signIn: "/login",
   },
